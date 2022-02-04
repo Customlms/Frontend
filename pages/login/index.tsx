1e64b1bda9283b61f79b3e4b06f512059364c156
YUN-GYEONG <champssong87@gmail.com>
2022-02-04 22:47:22 +0900

    2022-02-04 merge 충돌 수정

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,13 +1,49 @@
 import { NextPage } from "next";
-import { Button } from "@mui/material";
+import { Grid, Button, Container, TextField, Box } from "@mui/material";
 
-const Login: NextPage = () => {
+const login: NextPage = () => {
     return (
-        <div id="loginForm" className="loginForm">
-            <h1>Login 화면</h1>
-            <Button href="/find-user">signUp</Button>
-        </div>
+        <Container id="loginForm" className="loginForm" maxWidth="xs">
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    marginTop: 20
+                }}
+            >
+                <h1>Login 화면</h1>
+                <TextField
+                    label="ID"
+                    name="ID"
+                    required
+                    fullWidth
+                    sx={{ mb: 2 }}
+                />
+                <TextField
+                    label="Password"
+                    name="Password"
+                    type="password" 
+                    required
+                    fullWidth
+                    sx={{ mb: 2 }}
+                />
+                <Button
+                    id="loginButton"
+                    variant="contained"
+                    type = "submit"
+                    href="/main"
+                    fullWidth
+                >Sign In</Button>
+                <Grid container>
+                    <Grid item xs>
+                        <Button className="test" href="/find-user">Sign Up</Button>
+                    </Grid>
+                    <Button className="test" href="/find">Find Account</Button>
+                </Grid>
+            </Box>
+        </Container>
     )
 }
 
-export default Login;
+export default login;
